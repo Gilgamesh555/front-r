ef68bbc334f9cbc9bb78dbbeac2ee65bdd413a14
David9792 <Davit.el.unicodj@gmail.com>
2022-07-04 11:48:45 -0400

    ´merge´

--- src/app/administracion/Auxiliar.js
+++ src/app/administracion/Auxiliar.js
@@ -325,7 +325,9 @@ export class Auxiliar extends Component {
                     className="col-lg-3 btn btn-primary mr-2"
                   // onClick={onClickSearchButton}
                   >
-                    Buscar
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+</svg> Buscar
                   </button>
                   {
                     this.state.permissions !== undefined &&
@@ -335,7 +337,10 @@ export class Auxiliar extends Component {
                         className='col-lg-2 btn badge-success mr-2'
                         onClick={evt => this.registerAuxiliar(evt)}
                       >
-                        Registrar Nuevo
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-plus" viewBox="0 0 16 16">
+  <path d="M8.5 6a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V10a.5.5 0 0 0 1 0V8.5H10a.5.5 0 0 0 0-1H8.5V6z"/>
+  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/>
+</svg> Registrar Nuevo
                       </button>
                     )
                   }
